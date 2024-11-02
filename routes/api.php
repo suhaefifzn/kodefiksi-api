46430fbcf229b5b305b100000a1f743c7fedc20e
suhaefifzn <suhaefi21@gmail.com>
2024-11-02 18:20:09 +0700

    add: rate limiter for all routes

--- routes/api.php
+++ routes/api.php
@@ -9,92 +9,95 @@ use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Authentications
-Route::controller(AuthenticationController::class)
-    ->prefix('authentications')
+Route::middleware('throttle:120,1')
     ->group(function () {
-        Route::post('', 'login');
-        Route::delete('', 'logout')->middleware('auth.jwt');
-        Route::get('/check', 'check')->middleware('auth.jwt');
-    });
-
-// Users
-Route::controller(UserController::class)
-    ->prefix('users')
-    ->group(function () {
-        // all roles
-        Route::prefix('my')
-            ->middleware('auth.jwt')
+        Route::controller(AuthenticationController::class)
+            ->prefix('authentications')
             ->group(function () {
-                Route::get('/profile', 'getProfile');
-                Route::put('/profile', 'updateProfile');
-                Route::put('/password', 'updatePassword');
-                Route::post('/image', 'updateImage');
+                Route::post('', 'login');
+                Route::delete('', 'logout')->middleware('auth.jwt');
+                Route::get('/check', 'check')->middleware('auth.jwt');
             });
 
-        // admin only
-        Route::middleware('auth.admin')
+        // Users
+        Route::controller(UserController::class)
+            ->prefix('users')
             ->group(function () {
-                Route::get('', 'getUsers');
-                Route::post('', 'addUser');
-                Route::get('/{user:username}', 'getOneUser');
-                Route::delete('/{user:username}', 'deleteUser');
-                Route::put('/{user:username}', 'updateProfileUser');
-                Route::put('/{user:username}/password', 'updatePasswordUser');
-            });
-    });
+                // all roles
+                Route::prefix('my')
+                    ->middleware('auth.jwt')
+                    ->group(function () {
+                        Route::get('/profile', 'getProfile');
+                        Route::put('/profile', 'updateProfile');
+                        Route::put('/password', 'updatePassword');
+                        Route::post('/image', 'updateImage');
+                    });
 
-// Categories
-Route::controller(CategoryController::class)
-    ->prefix('categories')
-    ->group(function () {
-        // admin
-        Route::middleware('auth.admin')
-            ->group(function () {
-                Route::post('', 'addCategory');
-                Route::put('/{category:slug}', 'updateCategory');
-                Route::delete('/{category:slug}', 'deleteCategory');
-            });
-
-        // all roles
-        Route::middleware('auth.jwt')
-            ->group(function () {
-                Route::get('', 'getCategories');
-                Route::get('/{category:slug}', 'getOneCategory');
-            });
-    });
-
-// Articles
-Route::controller(ArticleController::class)
-    ->prefix('articles')
-    ->group(function () {
-        // public
-        Route::controller(PublicArticleController::class)
-            ->middleware('auth.client')
-            ->prefix('public')
-            ->group(function () {
-                Route::get('', 'getArticles');
-                Route::get('/{article:slug}', 'getOneArticle');
+                // admin only
+                Route::middleware('auth.admin')
+                    ->group(function () {
+                        Route::get('', 'getUsers');
+                        Route::post('', 'addUser');
+                        Route::get('/{user:username}', 'getOneUser');
+                        Route::delete('/{user:username}', 'deleteUser');
+                        Route::put('/{user:username}', 'updateProfileUser');
+                        Route::put('/{user:username}/password', 'updatePasswordUser');
+                    });
             });
 
-        // members
-        Route::middleware('auth.jwt')
+        // Categories
+        Route::controller(CategoryController::class)
+            ->prefix('categories')
             ->group(function () {
-                Route::get('', 'getArticles');
-                Route::post('', 'addArticle');
-                Route::post('/generate-slug', 'generateSlug');
-                Route::post('/upload-image', 'addImage');
-                Route::get('/images', 'getAllBodyImages');
-                Route::get('/stats', 'getStats');
-                Route::get('/{article:slug}', 'getOneArticle');
-                Route::put('/{article:slug}', 'editArticle');
-                Route::delete('/{article:slug}', 'deleteArticle');
+                // admin
+                Route::middleware('auth.admin')
+                    ->group(function () {
+                        Route::post('', 'addCategory');
+                        Route::put('/{category:slug}', 'updateCategory');
+                        Route::delete('/{category:slug}', 'deleteCategory');
+                    });
+
+                // all roles
+                Route::middleware('auth.jwt')
+                    ->group(function () {
+                        Route::get('', 'getCategories');
+                        Route::get('/{category:slug}', 'getOneCategory');
+                    });
             });
 
-        // Cache Control
-        Route::controller(CacheController::class)
-            ->prefix('cache')
-            ->middleware('auth.admin')
+        // Articles
+        Route::controller(ArticleController::class)
+            ->prefix('articles')
             ->group(function () {
-                Route::get('/flush', 'flushAll');
+                // public
+                Route::controller(PublicArticleController::class)
+                    ->middleware('auth.client')
+                    ->prefix('public')
+                    ->group(function () {
+                        Route::get('', 'getArticles');
+                        Route::get('/{article:slug}', 'getOneArticle');
+                    });
+
+                // members
+                Route::middleware('auth.jwt')
+                    ->group(function () {
+                        Route::get('', 'getArticles');
+                        Route::post('', 'addArticle');
+                        Route::post('/generate-slug', 'generateSlug');
+                        Route::post('/upload-image', 'addImage');
+                        Route::get('/images', 'getAllBodyImages');
+                        Route::get('/stats', 'getStats');
+                        Route::get('/{article:slug}', 'getOneArticle');
+                        Route::put('/{article:slug}', 'editArticle');
+                        Route::delete('/{article:slug}', 'deleteArticle');
+                    });
+
+                // Cache Control
+                Route::controller(CacheController::class)
+                    ->prefix('cache')
+                    ->middleware('auth.admin')
+                    ->group(function () {
+                        Route::get('/flush', 'flushAll');
+                    });
             });
     });
